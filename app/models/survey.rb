51d309dbe5acc75f14243f90250104d47bf0b4a0
shin <shin.php@gmail.com>
2012-06-13 10:41:20 +0800

    'validation'

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,4 @@
 class Survey < ActiveRecord::Base
   attr_accessible :company, :department, :email, :name, :phone, :q1, :q10, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9
+  validates_presence_of :company, :email, :name, :phone
 end
